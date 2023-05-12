64c980b18adf5e21c251bf31b801813efcf4b26b
Michael <usermike671@gmail.com>
2023-05-12 16:19:41 -0700

    added validator for updating controlled reflow

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -5,23 +5,45 @@ import styles from './cards.module.scss';
 const Cards = ({ cardCount, reflowOption }) => {
   const cardContainerRef = useRef();
   const [cardList, setCardList] = useState([]);
-
-  useEffect(() => {
-    if (reflowOption === 'Controlled') {
-      _renderPlaceholderCards()
-    } else {
-      _renderCardList();
-    }
-  }, [reflowOption])
+  const [placeholderCount, setPlaceholderCount] = useState(0);
 
   useEffect(() => {
     _renderCardList()
-  }, [cardCount])
+  }, [cardCount, reflowOption]);
+
+  useEffect(() => {
+    if (reflowOption === 'Controlled') {
+      _validatePlaceholders();
+    }
+  }, [cardContainerRef.current?.offsetWidth]);
+
+  const _validatePlaceholders = () => {
+    const currentCount = updateControlledCards();
+    if (currentCount > placeholderCount) {
+      return _renderCardList();
+    }
+    if (currentCount < placeholderCount) {
+      if (!currentCount) {
+        const tempCardListArray = cardList;
+        const newTempCardArray = tempCardListArray.slice(0, -placeholderCount);
+        setPlaceholderCount(currentCount);
+        return setCardList(newTempCardArray);
+      } else {
+        const tempCardListArray = cardList;
+        const cardCountToRemove = placeholderCount - currentCount;
+        setPlaceholderCount(currentCount);
+        const newTempCardArray = tempCardListArray.slice(0, -cardCountToRemove);
+        return setCardList(newTempCardArray);
+      }
+    } else {
+      return _renderCardList();
+    }
+  }
 
   // Placeholder(invisible) cards when in controlled reflow.
   const _cardPlaceHolderGenerator = () => {
     return (
-      <div className={styles.cardPlaceHolder} key={Math.floor(Math.random() * 100)} />
+      <div className={styles.cardPlaceHolder} key={Math.floor(Math.random() * 1000)} />
     )
   }
 
@@ -36,7 +58,6 @@ const Cards = ({ cardCount, reflowOption }) => {
     const containerWidth = Math.floor(cardContainerRef.current.offsetWidth);
     const maxCardsInRow = Math.floor(containerWidth / cardArea);
     const placeholderCount = divisibleGenerator(cardCount, maxCardsInRow);
-
     return placeholderCount;
   }
 
@@ -49,18 +70,25 @@ const Cards = ({ cardCount, reflowOption }) => {
         </div>
       )
     }
+    if (reflowOption === 'Controlled') {
+      return _renderPlaceholderCards(cardArray);
+    }
     return setCardList(cardArray);
   }
 
-  const _renderPlaceholderCards = () => {
-    let placeholderArray = [];
+  const _renderPlaceholderCards = (array) => {
+    let placeholderArray = array;
     let placeholderCount = updateControlledCards();
-    while (placeholderCount !== 0) {
-      placeholderArray.push(_cardPlaceHolderGenerator());
-      --placeholderCount;
+    setPlaceholderCount(placeholderCount);
+
+    if (!!placeholderCount) {
+      while (placeholderCount !== 0) {
+        placeholderArray.push(_cardPlaceHolderGenerator());
+        --placeholderCount;
+      }
     }
 
-    return setCardList([...cardList, ...placeholderArray])
+    return setCardList(placeholderArray);
   }
 
   return (
