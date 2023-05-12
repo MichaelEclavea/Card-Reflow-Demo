5b363e47016f9778af1cd501a5d95a02bf1f58f5
Michael <usermike671@gmail.com>
2023-05-11 18:16:15 -0700

    Updates

--- components/header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './header.module.scss';
-
-export const Header = () => {
-  return (
-    <header className={styles.appHeader}>
-      <h2>FlexBox Card Reflow Demo</h2>
-    </header>
-  )
-}
