daaa6e888048b276cb72c31b6ca7c84c8be4232a
Michael <usermike671@gmail.com>
2023-05-12 17:05:11 -0700

    added mobile view for control pane and cards

--- src/components/slider/SliderController.jsx
+++ src/components/slider/SliderController.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
 import { Slider } from '@fluentui/react/lib/Slider';
 import { Stack } from '@fluentui/react/lib/Stack';
 import { Label } from '@fluentui/react/lib/Label';
 import styles from './sliderController.module.scss';
 
 export const SliderController = ({ ariaValue, changeHandler, valueFormat }) => {
+  let windowSize = window.innerWidth;
+
+  useEffect(() => {
+    if (windowSize !== window.innerWidth) {
+      windowSize = window.innerWidth;
+    }
+    return () => windowSize = null;
+  }, [window.innerWidth])
+
   return (
     <div>
       <Label>
@@ -14,7 +24,7 @@ export const SliderController = ({ ariaValue, changeHandler, valueFormat }) => {
       <Slider
         className={styles.sliderControl}
         max={100}
-        min={7}
+        min={windowSize > 640 ? 7 : 20}
         defaultValue={100}
         ariaValueText={ariaValue}
         valueFormat={valueFormat}
